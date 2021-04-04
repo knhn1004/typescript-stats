7eca6bf44624697f1dfca6ebccf999f50ecbb2b5
oliver <oliver@pop-os.localdomain>
2021-04-04 13:45:29 +0800

    interface approach

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
+// create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-reader.read();
-const matches = reader.data;
+// create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     // wins as Home team
     manUnitedWins++;
